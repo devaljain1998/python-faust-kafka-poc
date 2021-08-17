fe9d6081a0200f7b6b17af8f7c1114ba0c3582db
John Doe <johndoe@email.com>
2021-08-17 20:58:04 +0530

    Changes for confluent integration

--- worker.py
+++ worker.py
@@ -3,9 +3,31 @@ import json
 import ast
 from typing import Optional, Type
 import time
+import socket
+import ccloud_lib
+import random
+import ssl
+import certifi
+
+ssl_context = ssl.create_default_context(
+    purpose=ssl.Purpose.SERVER_AUTH, cafile=certifi.where())
+
+config_file = './kafka.config'
+
+# New conf from CC-cloud
+conf = ccloud_lib.read_ccloud_config(config_file)
+
+# DEBUG
+print(conf)
 
 app = faust.App(
-    'faust_poc',
-    broker='faust-broker',
+    'kafka_streaming',
+    broker='pkc-41p56.asia-south1.gcp.confluent.cloud:9092',
+    broker_credentials=faust.SASLCredentials(
+        username=conf['sasl.username'],
+        password=conf['sasl.password'],
+        ssl_context=ssl_context
+    ),
     value_serializer='raw',
+    topic_replication_factor=3
 )        
